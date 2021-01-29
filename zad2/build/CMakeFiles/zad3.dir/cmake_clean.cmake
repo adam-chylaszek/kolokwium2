2144bb485ea1a0e8533c4f345f12a39b99b0f55b
Adam Chylaszek <adam.chylaszek@smcebi.edu.pl>
2021-01-29 16:48:29 +0100

    First commit

---
+++ zad2/build/CMakeFiles/zad3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zad3.dir/address.cpp.o"
+  "CMakeFiles/zad3.dir/employee.cpp.o"
+  "CMakeFiles/zad3.dir/main.cpp.o"
+  "zad3"
+  "zad3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zad3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
